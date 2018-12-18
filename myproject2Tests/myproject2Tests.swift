ebca47304ec23c700893e04d0bdd8ab267e7e025
Wu <wu@wudemacbook-air.local>
2018-12-18 13:55:55 +0800

    Initial Commit

---
+++ myproject2Tests/myproject2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  myproject2Tests.swift
+//  myproject2Tests
+//
+//  Created by student on 2018/10/24.
+//  Copyright © 2018年 sicnu. All rights reserved.
+//
+
+import XCTest
+@testable import myproject2
+
+class myproject2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
